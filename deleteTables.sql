9045213924bf40ca49c955b630eb469d2cc4a282
malgaran <137892514+malgaran@users.noreply.github.com>
2024-01-30 13:55:11 +0100

    delete files

---
+++ deleteTables.sql
@@ -0,0 +1,34 @@
+-- Drop the tables in reverse order to avoid dependency issues
+
+-- Drop table "istLeistungVon"
+DROP TABLE istLeistungVon;
+
+-- Drop table "RechnetAb"
+DROP TABLE RechnetAb;
+
+-- Drop table "Enthält"
+DROP TABLE Enthält;
+
+-- Drop table "Brief"
+DROP TABLE Brief;
+
+-- Drop table "Krankschreibung"
+DROP TABLE Krankschreibung;
+
+-- Drop table "Rezept"
+DROP TABLE Rezept;
+
+-- Drop table "Leistung"
+DROP TABLE Leistung;
+
+-- Drop table "Patient"
+DROP TABLE Patient;
+
+-- Drop table "Arzt"
+DROP TABLE Arzt;
+
+-- Drop table "Medikament"
+DROP TABLE Medikament;
+
+-- Drop table "Krankenversicherung"
+DROP TABLE Krankenversicherung;
